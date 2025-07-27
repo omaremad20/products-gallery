84687b8fee4d8ae7fd21e8b6034558b66ef81fd8
omaremad20 <oe747745@gmail.com>
2025-07-27 13:37:00 +0300

    edit vercel issues

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
